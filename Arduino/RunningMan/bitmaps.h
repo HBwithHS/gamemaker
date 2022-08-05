29d4c1bf1fd78b99eed20c58ecce8e4ee8decdeb
hjeon2k <hjeon2k@gmail.com>
2022-08-06 02:05:29 +0900

    init

---
+++ Arduino/RunningMan/bitmaps.h
@@ -0,0 +1,12 @@
+#include <avr/pgmspace.h>
+#ifndef BITMAPS_H
+#define BITMAPS_H
+
+extern const unsigned char arduino[];
+extern const unsigned char nightrun[];
+extern const unsigned char gameover[];
+
+extern const unsigned char* const char_anim[];
+extern const unsigned char* const enemy_anim[];
+
+#endif
